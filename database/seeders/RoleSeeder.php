4cfef1f1a465aa20b5929cfe3f6d9296f9add2d1
Sanskar Tiwari <sanskar.mk2@gmail.com>
2022-09-08 20:08:36 +0530

    shield seeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Enums\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +14,6 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        // Role
+        //
     }
 }
